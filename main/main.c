7173015d0fe8a47f2696506351d48cbd78278a45
Fabian Bartschke <fabian@bartschke.de>
2019-03-23 14:13:34 +0100

    main: cleanup includes

--- main/main.c
+++ main/main.c
@@ -26,11 +26,6 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
-#include "driver/i2c.h"
-
-#include <sys/socket.h>
-#include "esp_wifi.h"
-#include "esp_event_loop.h"
 
 #include "esp_log.h"
 #include "esp_err.h"
